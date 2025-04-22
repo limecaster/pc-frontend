4cf26c3c885074deb1f04a72b7cb7201f5dc0aa5
Đỗ Văn Bâng <bang.dovan38@gmail.com>
2025-04-22 23:53:34 +0700

    update backend url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 // Make sure this URL points to your running NestJS server
 export const API_URL =
-    process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+    process.env.NEXT_PUBLIC_API_URL || "https://bpcstore.me/api";
 
 // Other constants can be added here
